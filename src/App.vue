9ab89d05e3dd395485935a33aa39509932f79ef5
calvin <puram.calvin@gmail.com>
2020-04-24 08:55:56 +0100

    app cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,5 @@
 <template>
   <div>
-    <div class="text-center m-3">
-      <Navbar />
-    </div>
-
-    <router-view />
+    App
   </div>
 </template>
-
-<script>
-import Navbar from './components/Navbar'
-export default {
-  name: 'App',
-  components: {
-    Navbar
-  }
-}
-</script>
-
-<style>
-body {
-  text-align: center;
-  background: #f4f4f4;
-}
-a {
-  color: #333;
-  text-decoration: none !important;
-}
-</style>
--- src/views/EventCreate.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Create An Event</h1>
-</template>
