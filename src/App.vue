78df8413f95a3333db00517dce699e67766336fc
calvin <puram.calvin@gmail.com>
2020-04-25 11:04:52 +0100

    set up axios

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,24 @@
     App
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      positiveCase: [],
+      hospitalizedCases: [],
+      arrInIcu: [],
+      arrInVentilators: [],
+      arrRecover: [],
+      arrDeaths: []
+    }
+  },
+  async created() {
+    const res = await axios.get('https://covidtracking.com/api/us/daily')
+    console.log(res.data)
+  }
+}
+</script>
