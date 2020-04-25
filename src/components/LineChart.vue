be2c197eeef27d140e58efa6178f73dc5819d5a5
calvin <puram.calvin@gmail.com>
2020-04-25 12:26:44 +0100

    created chart component

---
+++ src/components/LineChart.vue
@@ -0,0 +1,32 @@
+<script>
+import { Line } from 'vue-chartjs'
+export default {
+  extends: Line,
+  props: {
+    labels: {
+      type: String
+    },
+    chartdata: {
+      type: Array
+    },
+    options: {
+      type: Object
+    }
+  },
+  mounted() {
+    const dates = this.chartdata.map(info => info.date).reverse()
+    const totals = this.chartdata.map(info => info.total).reverse()
+
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: {
+          labels: dates,
+          data: totals
+        }
+      },
+      this.options
+    )
+  }
+}
+</script>
