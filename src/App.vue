9a5493dfe26824fcd4fcc4795aa4494c9190f643
calvin <puram.calvin@gmail.com>
2020-04-25 11:46:37 +0100

    created simplified data array

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 import axios from 'axios'
-
+import moment from 'moment'
 export default {
   data() {
     return {
@@ -19,8 +19,30 @@ export default {
     }
   },
   async created() {
-    const res = await axios.get('https://covidtracking.com/api/us/daily')
-    console.log(res.data)
+    const { data } = await axios.get('https://covidtracking.com/api/us/daily')
+    data.forEach(info => {
+      const date = moment(info.date, 'YYYYMMDD').format('MM/DD')
+      const {
+        positive,
+        hospitalized,
+        inIcuCumulative,
+        onVentilatorCumulative,
+        recovered,
+        death
+      } = info
+
+      if (data.length > 0) {
+        this.positiveCase.push({ date, total: positive })
+        this.hospitalizedCases.push({ date, total: hospitalized })
+        this.arrInIcu.push({ date, total: inIcuCumulative })
+        this.arrInVentilators.push({ date, total: onVentilatorCumulative })
+        this.arrRecover.push({
+          date,
+          total: recovered
+        })
+        this.arrDeaths.push({ date, total: death })
+      }
+    })
   }
 }
 </script>
